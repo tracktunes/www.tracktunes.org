6731cfde8d5f8dcae00c67f7f7081659e8e1639f
Doron Tal <5935957+dorontal@users.noreply.github.com>
2021-08-14 23:44:43 -0400

    added not yet working because copied from library

---
+++ scripts/backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+MY_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+pushd $MY_DIR > /dev/null 2>&1
+
+cd ..; P="`pwd`"; PROJECT_HOME=`basename $P`; cd $MY_DIR
+
+BACKUP_DIR="$HOME/BACKUPS/$PROJECT_HOME/`date +%F`"
+
+mkdir -p "$BACKUP_DIR"
+
+sudo mount-backup
+
+TMP_DIR="$MY_DIR/../../$RANDOM"
+
+mkdir $TMP_DIR
+
+mv ../amplify $TMP_DIR
+mv ../node_modules $TMP_DIR
+mv ../.git $TMP_DIR
+
+mkdir -p $BACKUP_DIR
+
+rsync -avb .. $BACKUP_DIR
+
+# restore
+mv $TMP_DIR/amplify ..
+mv $TMP_DIR/node_modules ..
+mv $TMP_DIR/.git ..
+rmdir $TMP_DIR
+
+popd > /dev/null 2>&1
