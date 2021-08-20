f7872e16615d0cb8824fe82c4812175a67b66acd
Doron Tal <5935957+dorontal@users.noreply.github.com>
2021-08-20 10:41:50 -0400

    minor

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-home',
@@ -8,7 +9,11 @@ import { Component } from '@angular/core';
 export class HomePage {
     public bShowLearnMore: boolean = false;
     
-    constructor() {}
+    constructor(private router: Router) {}
+
+    public onClickSignUp(): void {
+        this.router.navigate([ 'thank-you' ]);
+    }
 
     public onClickLearnMore(): void {
         this.bShowLearnMore = true;
