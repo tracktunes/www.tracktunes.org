f4c891852df521cbd0186fa3a6df86913916e12f
Doron Tal <5935957+dorontal@users.noreply.github.com>
2021-08-20 14:56:20 -0400

    minor

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth } from 'aws-amplify';
+// import { verifier } from 'email-verify';
+// import { dns } from 'dns';
+
+const EMAIL_REG_EXP: RegExp =
+      /^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/;
+const EMAIL_MAX_LEN: number = 255;
+const EMAIL_MIN_LEN: number = 8;
 
 @Component({
     selector: 'app-home',
@@ -8,10 +16,35 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
     public bShowLearnMore: boolean = false;
-    
+    public email: string = null;
+
     constructor(private router: Router) { }
 
     public onClickSignUp(): void {
+        const len: number = this.email.length;
+        let bValid: boolean = false;
+        if (!(len > EMAIL_MAX_LEN || len < EMAIL_MIN_LEN ||
+              this.email[0] === '.' || this.email[len-1] === '.')) {
+            bValid = EMAIL_REG_EXP.test(this.email);
+        }
+        /*
+        if (bValid) {
+            verifier.verify(this.email, (err, info) => {
+                if (info.success) {
+                    bValid = true;
+                }
+            });
+        }
+        console.log('email: ' + this.email, bValid);
+        */
+        /*
+        public signUp(email: string, password: string): Promise<ISignUpResult> {
+        return Auth.signUp({
+            username: email,
+            password,
+            attributes: { email }
+        });
+        */
         this.router.navigate([ 'thank-you' ]);
     }
 
