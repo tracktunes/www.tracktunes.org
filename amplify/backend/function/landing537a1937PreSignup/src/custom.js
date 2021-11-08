d8a2a7bfc355cacdb8c23e97566d70db0a27808c
Doron Tal <5935957+dorontal@users.noreply.github.com>
2021-11-08 17:06:35 -0500

    minor

--- amplify/backend/function/landing537a1937PreSignup/src/custom.js
+++ amplify/backend/function/landing537a1937PreSignup/src/custom.js
@@ -1,4 +1,4 @@
-exports.handler = (event, context, callback) => {
-  // insert code to be executed by your lambda trigger
-  callback(null, event);
+exports.handler = async event => {
+    // insert code to be executed by your lambda trigger
+    console.log('here we go! ' + JSON.stringify(event, null, 2));
 };
