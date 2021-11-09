43fe4bc5ff92d938fb76d43c860a87936c6148c1
Doron Tal <5935957+dorontal@users.noreply.github.com>
2021-11-08 20:47:48 -0500

    minor

--- amplify/backend/function/landing537a1937PreSignup/src/custom.js
+++ amplify/backend/function/landing537a1937PreSignup/src/custom.js
@@ -1,4 +1,16 @@
+const validateEmail = require('validate_email');
+
 exports.handler = async event => {
     // insert code to be executed by your lambda trigger
-    console.log('here we go! ' + JSON.stringify(event, null, 2));
+    const email = event.request.userAttributes.email;
+    const dateString = new Date().toUTCString();
+    if (validateEmail(email)) {
+        // event.response.autoConfirmUser = true;
+        // event.response.autoVerifyEmail = true;
+        console.log(dateString + ' - email: ' + email);        
+    } else {
+        console.log(dateString + ' - bad: ' + email);
+        throw Error('Email not valid');
+    }
+    return event;
 };
