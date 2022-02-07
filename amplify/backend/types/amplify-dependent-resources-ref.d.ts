7de9bbb6fdb5a719ca0eed23afe24cb44eccd574
Doron Tal <doron@tracktunes.org>
2022-02-07 14:55:25 -0500

    minor

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,22 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "landingb2fe178c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "function": {
+        "landingb2fe178cPreSignup": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    }
+}
--- scripts/reamplify_landing.sh
+++ scripts/reamplify_landing.sh
@@ -2,13 +2,6 @@
 
 MY_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-BACKUP_DIR="$1"
-
-if [ "$BACKUP_DIR" == "" ]; then
-    echo "No argument provided: please provide lambda sources backup directory"
-    exit 1
-fi
-
 $MY_DIR/backup_amplify.sh
 
 $MY_DIR/amplify_delete.exp
@@ -20,8 +13,6 @@ $MY_DIR/amplify_init.exp
 $MY_DIR/amplify_add_auth.exp
 
 $MY_DIR/amplify_auth_update.exp
-
-# $MY_DIR/rebuild_lambdas.sh $BACKUP_DIR
 
 $MY_DIR/amplify_push.exp
 
