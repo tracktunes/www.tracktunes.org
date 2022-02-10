ef9d0201841b2355b8f9fd3454b876ec1631189a
Doron Tal <doron@tracktunes.org>
2022-02-10 17:12:20 -0500

    minor

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { Amplify } from 'aws-amplify';
+import aws_exports from './aws-exports';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
+Amplify.configure(aws_exports);
+
+// Supported log levels: ERROR, WARN, INFO, DEBUG, VERBOSE
+// Amplify.Logger.LOG_LEVEL = 'DEBUG';
+
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+    .catch(err => console.log('Error caught in main: ' + err));
