d2ddd1db2d8a541bfc15ca0d50ff7bc2ff6b0e0f
Doron Tal <doron@tracktunes.org>
2022-02-10 12:46:20 -0500

    (0.0.4) deployed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -11,7 +11,10 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+      { provide: LocationStrategy, useClass: HashLocationStrategy },
+],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
