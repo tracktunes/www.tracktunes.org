b69120c5f627cd2efcea966ffdddcb67f5e144d7
Doron Tal <doron@tracktunes.org>
2022-03-30 16:21:43 -0400

    minor

---
+++ docs/9072.c9beb3bd1b1dbe07.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[9072],{9072:(a,l,s)=>{s.r(l),s.d(l,{ion_text:()=>n});var o=s(8888),c=s(318),e=s(7853);const n=class{constructor(t){(0,o.r)(this,t)}render(){const t=(0,c.b)(this);return(0,o.h)(o.H,{class:(0,e.c)(this.color,{[t]:!0})},(0,o.h)("slot",null))}};n.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
